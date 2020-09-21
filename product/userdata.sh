76c7eb7ae2ac4f78db70c60f03fec01218832f1e
EC2 Default User <ec2-user@ip-172-31-80-237.ec2.internal>
2020-09-21 14:36:03 +0000

    fixed startup product

--- product/userdata.sh
+++ product/userdata.sh
@@ -2,8 +2,11 @@
 yum -y install git
 git clone https://github.com/mohanraz81/archeplay-ecomm-demo.git
 awsregion=us-east-1
-tablename=mytable
+tablename=producttable
 cd archeplay-ecomm-demo/product
 sed -i "s/REGIONNAME/$awsregion/g" docker-compose.yaml
 sed -i "s/TABLENAME/$tablename/g" docker-compose.yaml
-sh -x ./startup.sh
+cp product.service /etc/systemd/system/product.service
+systemctl daemon-reload
+systemctl enable product.service
+systemctl start product.service
