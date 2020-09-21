4ffb2656a6f87b787d5cca3106dea6a481570d6e
EC2 Default User <ec2-user@ip-172-31-80-237.ec2.internal>
2020-09-21 10:51:26 +0000

    test

--- product/startup.sh
+++ product/startup.sh
@@ -7,6 +7,4 @@ systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sed -i "s/REGIONNAME/$awsregion/g" docker-compose.yaml
-sed -i "s/TABLENAME/$tablename/g" docker-compose.yaml
 docker-compose up -d
