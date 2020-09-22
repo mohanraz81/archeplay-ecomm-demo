70da7b502f58c3dce7733b7900a93ee36672765c
EC2 Default User <ec2-user@ip-172-31-80-237.ec2.internal>
2020-09-22 16:47:49 +0000

    fixed Id in priduct

--- product/app.py
+++ product/app.py
@@ -31,140 +31,140 @@ def dumpproduct():
   table=dynamodb.Table(TableName)
   items = [
       {
-      "Id": "skagen_skw6327",
+      "id": "skagen_skw6327",
       "title": "Skagen",
       "description": "Skagen Men's SKW6327 Hagen Stainless Steel Mesh Watch",
       "image": "skagen_skw6327.png",
       "Price": "100"
   },
   {
-      "Id": "skagen_titanium",
+      "id": "skagen_titanium",
       "title": "Skagen",
       "description": "Skagen Connected Men's Holst Titanium Hybrid Smartwatch",
       "image": "skagen_titanium.png",
       "Price": "120"
   },
   {
-      "Id": "fossil_smartwatch_gen4",
+      "id": "fossil_smartwatch_gen4",
       "title": "Fossil Smartwatch",
       "description": "Fossil Men's Gen 4 Explorist HR Stainless Steel Touchscreen Smartwatch with Heart Rate, GPS, NFC, and Smartphone Notifications",
       "image": "fossil_smartwatch_gen4.png",
       "Price": "100"
   },
   {
-      "Id": "fossil_nate_stainless",
+      "id": "fossil_nate_stainless",
       "title": "Fossil Nate Stainless",
       "description": "Fossil Men's Nate Stainless Steel Hybrid Smartwatch with Activity Tracking and Smartphone Notifications",
       "image": "fossil_nate_stainless.png",
       "Price": "110"
   },
   {
-      "Id": "apple_series5",
+      "id": "apple_series5",
       "title": "Apple Series 5",
       "description": "Apple Watch Series 5 (GPS, 40mm) - Space Gray Aluminum Case with Black Sport Band",
       "image": "apple_series5.png",
       "Price": "130"
   },
   {
-      "Id": "samsung_smartwatch",
+      "id": "samsung_smartwatch",
       "title": "Samsung smartwatch",
       "description": "Samsung Galaxy Smartwatch (46mm) Bluetooth - Silver/Black",
       "image": "samsung_smartwatch.png",
       "Price": "140"
   },
   {
-      "Id": "tagheuer_carrera",
+      "id": "tagheuer_carrera",
       "title": "Tag Heuer",
       "description": "Mens Tag heuer Carrera Calibre Heuer 01 Automatic Chronograph 45 MM CAR2A1W.BA0703",
       "image": "tagheuer_carrera.png",
       "Price": "100"
   },
   {
-      "Id": "Casio_Analog",
+      "id": "Casio_Analog",
       "title": "Casio",
       "description": "Casio Analog Black Dial Men's Watch-MTP-VT01L-1BUDF (A1615)",
       "image": "Casio Analog Black Dial Men's Watch-MTP-VT01L-1BUDF (A1615).PNG",
       "Price": "140"
   },
   {
-      "Id": "Casio_Enticer",
+      "id": "Casio_Enticer",
       "title": "Casio",
       "description": "Casio Enticer Analog Black Dial Men's Watch - MTP-VD01G-1BVUDF (A1367)",
       "image": "Casio Enticer Analog Black Dial Men's Watch - MTP-VD01G-1BVUDF (A1367).PNG",
       "Price": "200"
   },
   {
-      "Id": "Casio_Enticer_Analog",
+      "id": "Casio_Enticer_Analog",
       "title": "Casio",
       "description": "Casio Enticer Analog Blue Dial Men's Watch - MTP-1314D-2AVDF (A551)",
       "image": "Casio Enticer Analog Blue Dial Men's Watch - MTP-1314D-2AVDF (A551).PNG",
       "Price": "300"
   },
   {
-      "Id": "Casio_Enticer_Blue",
+      "id": "Casio_Enticer_Blue",
       "title": "Casio",
       "description": "Casio Enticer Analog Blue Dial Men's Watch - MTP-VD01D-2EVUDF (A1364)",
       "image": "Casio Enticer Analog Blue Dial Men's Watch - MTP-VD01D-2EVUDF (A1364).PNG",
       "Price": "250"
   },
   {
-      "Id": "Casio_Enticer_Silver",
+      "id": "Casio_Enticer_Silver",
       "title": "Casio",
       "description": "Casio Enticer Analog Silver Dial Men's Watch - MTP-V001D-7BUDF (A1082)",
       "image": "Casio Enticer Analog Silver Dial Men's Watch - MTP-V001D-7BUDF (A1082).PNG",
       "Price": "100"
   },
   {
-      "Id": "Casio_Chronograph",
+      "id": "Casio_Chronograph",
       "title": "Casio",
       "description": "Casio Enticer Chronograph Black Dial Men's Watch - MTP-1374L-1AVDF (A834)",
       "image": "Casio Enticer Chronograph Black Dial Men's Watch - MTP-1374L-1AVDF (A834).PNG",
       "Price": "200"
   },
   {
-      "Id": "Casio_Youth_Combination",
+      "id": "Casio_Youth_Combination",
       "title": "Casio",
       "description": "Casio Youth-Combination Analog-Digital Gold Dial Men's Watch - AEQ-110BW-9AVDF (AD206)",
       "image": "Casio Youth-Combination Analog-Digital Gold Dial Men's Watch - AEQ-110BW-9AVDF (AD206).PNG",
       "Price": "200"
   },
   {
-      "Id": "casio-MTP-v300D",
+      "id": "casio-MTP-v300D",
       "title": "Casio",
       "description": "casio-MTP-v300D-1AUDF(A1173)",
       "image": "casio-MTP-v300D-1AUDF(A1173).PNG",
       "Price": "200"
   },
   {
-      "Id": "casio-MTP-v300D-7AUDF",
+      "id": "casio-MTP-v300D-7AUDF",
       "title": "Casio",
       "description": "casio-MTP-v300D-7AUDF(A1177)",
       "image": "casio-MTP-v300D-7AUDF(A1177).PNG",
       "Price": "200"
   },
   {
-      "Id": "casio-MTP-vD01L-1EVUDF",
+      "id": "casio-MTP-vD01L-1EVUDF",
       "title": "Casio",
       "description": "casio-MTP-vD01L-1EVUDF",
       "image": "casio-MTP-vD01L-1EVUDF.PNG",
       "Price": "120"
   },
   {
-      "Id": "scuderia_ferrari",
+      "id": "scuderia_ferrari",
       "title": "Scuderia Ferrari",
       "description": "Scuderia Ferrari Men's Stainless Steel Quartz Watch with Silicone Strap, Black, 24 (Model: 830344)",
       "image": "scuderia_ferrari.png",
       "Price": "100"
   },
   {
-      "Id": "armani_exchange",
+      "id": "armani_exchange",
       "title": "Armani Exchange",
       "description": "Armani Exchange Men's Classic Stainless Steel Watch",
       "image": "armani_exchange.png",
       "Price": "200"
   },
   {
-      "Id": "casio_vintage_a168wa",
+      "id": "casio_vintage_a168wa",
       "title": "Casio Vintage",
       "description": "Casio Men's Vintage A168WA-1 Electro Luminescence Watch",
       "image": "casio_vintage_a168wa.png",
