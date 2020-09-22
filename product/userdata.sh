e6fd80ea1219244fc66127c83ffa249e3ab1737b
EC2 Default User <ec2-user@ip-172-31-80-237.ec2.internal>
2020-09-22 11:20:51 +0000

    test

--- product/userdata.sh
+++ product/userdata.sh
@@ -9,11 +9,9 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 yum -y install git
 git clone https://github.com/mohanraz81/archeplay-ecomm-demo.git
-awsregion=us-east-1
-tablename=producttable
 cd archeplay-ecomm-demo/product
-sed -i "s/REGIONNAME/$awsregion/g" docker-compose.yaml
-sed -i "s/TABLENAME/$tablename/g" docker-compose.yaml
+sed -i "s/REGIONNAME/${ AWS::Region }/g" docker-compose.yaml
+sed -i "s/TABLENAME/${ TABLENAME }/g" docker-compose.yaml
 cp product.service /etc/systemd/system/product.service
 systemctl daemon-reload
 systemctl enable product.service
